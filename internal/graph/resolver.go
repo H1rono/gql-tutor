590a233b3713b797281bbd820fcc75e78a016703
H1rono <hronok66@gmail.com>
2025-07-19 14:27:59 +0900

    :sparkles: Add `*service.Service` to `Resolver`

--- internal/graph/resolver.go
+++ internal/graph/resolver.go
@@ -1,7 +1,17 @@
 package graph
 
+import "github.com/h1rono/gql-tutor/internal/service"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	s *service.Service
+}
+
+func NewResolver(s *service.Service) *Resolver {
+	return &Resolver{
+		s: s,
+	}
+}
