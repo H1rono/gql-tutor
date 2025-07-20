5daaad9b209bd2f077dde5c6e69c0ddf1fc280eb
H1rono <hronok66@gmail.com>
2025-07-20 13:44:01 +0900

    :wrench: Enable versioned migration feature of ent

--- internal/generate.go
+++ internal/generate.go
@@ -1,4 +1,4 @@
 package internal
 
 //go:generate go tool gqlgen generate
-//go:generate go tool ent generate ./ent/schema
+//go:generate go tool ent generate --feature sql/versioned-migration ./ent/schema
